0f8b2b7b71901a664606d06dcf8d38e3985eaabc
Nguyễn An Đức <anduc061104@gmail.com>
2025-10-05 11:26:03 +0700

    Config udating database

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"  # SQLite file app.db
+        "postgresql+psycopg2://postgres:ducan06112004@localhost:5432/cinema_db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
